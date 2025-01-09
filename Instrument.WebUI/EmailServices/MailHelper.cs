202eaa9cd0410032eb838d51c4ac0dba82d0fb32
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-01-09 21:47:57 +0300

    add to EmailServices

---
+++ Instrument.WebUI/EmailServices/MailHelper.cs
@@ -0,0 +1,46 @@
+﻿namespace Instrument.WebUI.EmailServices
+{
+	public static class MailHelper
+	{
+		public static bool SenEmail(string body,string to,string subject,bool isHtml = true)
+		{
+			return SendEmail(body, new List<string> { to }, subject, isHtml);
+		}
+
+		private static bool SendEmail(string body, List<string> to, string subject, bool isHtml)
+		{
+			bool result = false;
+
+			try
+			{
+				var message = new MailMessage();
+				message.From = new MailAddress("instrumenthubmailservices@gmail.com");
+
+				to.ForEach(x =>
+				{
+					message.To.Add(new MailAddress(x));
+				});
+
+				message.Subject = subject;
+				message.Body = body;
+				message.IsBodyHtml = isHtml;
+
+				using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+				{
+					smtp.EnableSsl = true;
+					smtp.Credentials = new NetworkCredential("instrumenthubmailservices@gmail.com", "wdpy prpp pekv nfll");
+					smtp.UseDefaultCredentials = false;
+					smtp.Send(message);
+					result = true;
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				result = false;
+			}
+
+			return result;
+		}
+	}
+}
