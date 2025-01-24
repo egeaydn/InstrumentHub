ce04e825ca6879030abffbdd57d96422d88cb7aa
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-01-24 14:34:25 +0300

    Manage Page Fixed

--- InstrumentHub.WebUI/Models/AccountModel.cs
+++ InstrumentHub.WebUI/Models/AccountModel.cs
@@ -5,7 +5,7 @@ namespace Instrument.WebUI.Models
 	public class AccountModel : AplicationUser
 	{
 		public string FullName { get; set; }
-		public string Password { get; set; }
+		//public string Password { get; set; }
 		public string Email { get; set; }
 		public string UserName { get; set; }
 	}
