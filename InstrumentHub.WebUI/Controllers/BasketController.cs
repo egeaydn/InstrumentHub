cf2ad67f0c9dd071c31759fb5e8a8b5a54fe69ab
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-01-30 20:54:28 +0300

    add to Basketcontroller.cs and home.cshtml

---
+++ InstrumentHub.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,29 @@
+﻿using Instrument.Business.Abstract;
+using Instrument.WebUI.Identity;
+using InstrumentHub.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstrumentHub.WebUI.Controllers
+{
+	public class BasketController : Controller
+	{
+		private ICartServices _cartService;
+		private IEProductServices _productService;
+		private IOrderServices _orderService;
+		private List<AplicationUser> _usermanager;
+
+		public BasketController(ICartServices cartServices,IEProductServices eProductServices,IOrderServices orderServices,List<AplicationUser> usermanager)
+		{
+			_cartService = cartServices;
+			_productService = eProductServices;
+			_orderService = orderServices;
+			_usermanager = usermanager;
+		}
+
+		public IActionResult Home()
+		{
+
+			return View();
+		}
+	}
+}
