aa333c2ea741af9a591dab304d27a9a94b3b5dd0
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-02-09 17:01:13 +0300

    Checkoot [HttpPost] IactionResult ,SaveOrder and PaymentProces in BaskrtController

--- InstrumentHub.WebUI/Controllers/BasketController.cs
+++ InstrumentHub.WebUI/Controllers/BasketController.cs
@@ -3,8 +3,10 @@ using Instrument.Business.Abstract;
 using Instrument.WebUI.Identity;
 using Instrument.WebUI.Models;
 using InstrumentHub.Entites;
+using InstrumentHub.WebUI.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace InstrumentHub.WebUI.Controllers
 {
@@ -89,6 +91,160 @@ namespace InstrumentHub.WebUI.Controllers
 				}).ToList()
 			};
 			return View(orderModel);
+		}
+
+		[HttpPost]
+		public IActionResult Checkout(OrderModel model, string paymentMethod)
+		{
+			ModelState.Remove("CartModel");
+
+			if (ModelState.IsValid)
+			{
+				var userId = _usermanager.GetUserId(User);
+				var cart = _cartService.GetCartByUserId(userId);
+
+				model.CartTemplate = new CartModel()
+				{
+					CartId = cart.Id,
+					CartItems = cart.CartItems.Select(x => new CartItemModel()
+					{
+						CartItemId = x.Id,
+						ProductId = x.EProduct.Id,
+						ProductName = x.EProduct.Name,
+						Price = x.EProduct.Price,
+						Image = x.EProduct.Images[0].ImageUrl,
+						Quantity = x.Quantity
+					}).ToList()
+				};
+
+				if (paymentMethod == "credit")
+				{
+					var payment = PaymentProces(model);
+
+					if (payment.Result.Status == "success")
+					{
+						SaveOrder(model, payment, userId);
+						ClearCart(cart.Id.ToString());
+						TempData.Put("message", new ResultMessageModel()
+						{
+							Title = "Order Completed",
+							Message = "Your order has been completed successfully",
+							Css = "success"
+						});
+					}
+					else
+					{
+						TempData.Put("message", new ResultMessageModel()
+						{
+							Title = "Order Uncompleted",
+							Message = "Your order could not be completed",
+							Css = "danger"
+						});
+					}
+				}
+				else
+				{
+
+					SaveOrder(model, userId);
+					ClearCart(cart.Id.ToString());
+
+					TempData.Put("message", new ResultMessageModel()
+					{
+						Title = "Order Completed",
+						Message = "Your order has been completed successfully",
+						Css = "success"
+					});
+
+				}
+
+			}
+
+			return View(model);
+		}
+
+		private void ClearCart(string id)
+		{
+			_cartService.ClearCart(id);
+		}
+
+		//Burası Eft Kısmı
+
+		private void SaveOrder(OrderModel model, object payment, string userId)
+		{
+			Order order = new Order()
+			{
+				OrderNumber = Guid.NewGuid().ToString(),
+				OrderEnums = OrderStatus.completed,
+				PaymentEnum = PaymentMethod.Eft,
+				PaymentToken = Guid.NewGuid().ToString(),
+				ConversionId = Guid.NewGuid().ToString(),
+				PaymentId = Guid.NewGuid().ToString(),
+				OrderNote = model.OrderNote,
+				Orderdate = DateTime.Now,
+				FirstName = model.FirstName,
+				Lastname = model.LastName,
+				Adress = model.Adress,
+				City = model.City,
+				Email = model.Email,
+				Phone = model.Phone,
+				UserId = userId
+			};
+
+			foreach (var cartItem in model.CartTemplate.CartItems)
+			{
+				var orderItem = new Entites.OrderItem()
+				{
+					Price = cartItem.Price,
+					Quantity = cartItem.Quantity,
+					EProductId = cartItem.ProductId,
+				};
+
+				order.OrderItems.Add(orderItem);
+			}
+
+			_orderService.Create(order);
+
+		}
+
+		private object PaymentProces(OrderModel model)
+		{
+			var userId = _usermanager.GetUserId(User);
+			var orders = _orderService.GetOrders(userId);
+
+			var orderListModel = new List<OrderListeModel>();
+
+			OrderListeModel orderModel;
+
+			foreach (var order in orders)
+			{
+
+				orderModel = new OrderListeModel();
+				orderModel.id = order.Id;
+				orderModel.Adress = order.Adress;
+				orderModel.OrderNumber = order.OrderNumber;
+				orderModel.OrderDate = order.Orderdate;
+				orderModel.OrderState = order.OrderEnums;
+				orderModel.PaymentTypes = order.PaymentEnum;
+				orderModel.OrderNote = order.OrderNote;
+				orderModel.City = order.City;
+				orderModel.Email = order.Email;
+				orderModel.FirstName = order.FirstName;
+				orderModel.LastName = order.Lastname;
+				orderModel.Phone = order.Phone;
+
+				orderModel.OrderItems = order.OrderItems.Select(i => new OrderItemCartItemModel()
+				{
+					OrderItemId = i.Id,
+					ProductName = i.Eproduct.Name,
+					Price = i.Eproduct.Price,
+					Quantity = i.Quantity,
+					ImageUrl = i.Eproduct.Images[0].ImageUrl
+				}).ToList();
+
+				orderListModel.Add(orderModel);
+			}
+
+			return View(orderListModel);
 		}
 	}
 }
